2964d52dc26a18ba7499ddb64f5fbcceff1a9ee6
zesty <nikkolas.paserba@outlook.com>
2021-05-24 10:05:16 -0400

    Updated knobgoblin testing keymap

--- keyboards/knobgoblin/keymaps/Test/keymap.c
+++ keyboards/knobgoblin/keymaps/Test/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   MSISO,  	MSBACK,   	KC_NO,  	KC_NO,
                   MSLEFT,  	MSTOP,   	MSRIGHT,	KC_NO,
          TEST, 	  KC_NO,  	MSFRONT,   	MSBOT,  	KC_ENT,
-         KC_MUTE, MSFIT,  	MSFIT,   	KC_NO, 		KC_ENT
+         KC_MUTE, MSFIT,  	MSFIT,   	KC_DEL,		KC_ENT
 				 ),
 				 
 	[2] = LAYOUT_ortho(
